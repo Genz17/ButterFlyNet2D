2914f5ab94333cad5006d6fd8f172f1e9a177e39
Genz17 <genzyang17@gmail.com>
2022-07-05 22:33:45 +0800

    testing part

--- Test/Test.py
+++ Test/Test.py
@@ -1,19 +1,51 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(__file__,'..','..','Funcs')))
+sys.path.append(os.path.abspath(os.path.join(__file__,'..','..','Nets')))
 import torch
+import torchvision
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from Mask import squareMask64,lineMask256,squareMask128,squareMask256
+import numpy as np
+from ButterFlyNet_INPAINT import ButterFlyNet_INPAINT
+from ButterFlyNet_DENOISE import ButterFlyNet_DENOISE
+from inpaint_test_func import *
+from denoise_test_func import *
 
-def test(loss_func, Net, test_loader,test_acc):
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for step, (image, label) in enumerate(test_loader):
-            image = image.cuda()
-            label = label.cuda()
-            output = Net(image)
-            test_loss = test_loss + loss_func(output, label).item()
-            pred = torch.max(output.detach(), 1)[1]
-            correct = correct + pred.eq(label.detach().view_as(pred)).sum()
-        test_loss = test_loss/len(test_loader.dataset)
-        test_acc.append((correct/len(test_loader.dataset)).item())
-        print('\nTest set: Avg. loss:\t{:.4f}, Accuracy: {}/{} ({:.6f}%)\n'.format(test_loss,
-                                                                                   correct,
-                                                                                   len(test_loader.dataset),
-                                                                                   100*correct/len(test_loader.dataset)))
+testTypeList = ['inpainting','denoising','deblurring']
+testType = testTypeList[0]
+
+batch_size = 256
+local_size = 64
+image_size = 256
+data_path = '../../CELEBA/'
+net_layer = 6 # should be no more than log_2(local_size)
+cheb_num = 4
+local_size = 32
+if testType == 'inpainting':
+    Net = ButterFlyNet_INPAINT(local_size, net_layer, cheb_num, False).cuda()
+elif testType == 'denoising':
+    Net = ButterFlyNet_DENOISE(local_size, net_layer, cheb_num, False).cuda()
+elif testType == 'deblurring':
+    pass
+test_loader = DataLoader(
+    torchvision.datasets.ImageFolder(root=data_path,
+                               transform=torchvision.transforms.Compose(
+                                   [torchvision.transforms.ToTensor(),
+                                    torchvision.transforms.Resize((image_size,image_size))])),
+    batch_size=batch_size, shuffle=False)
+
+
+print('Loading parameters...')
+Net.load_state_dict(torch.load('../../PTHS/54GRAYLineSeperateCelebainpainting.pth'))
+print('Done.')
+
+if testType == 'inpainting':
+    test_inpainting(test_loader,batch_size,Net,eval('squareMask'+str(image_size))(torch.zeros(batch_size,1,image_size,image_size)).cuda(),image_size,local_size)
+elif testType == 'denoising':
+    noise_mean = 0
+    noise_std = 0.1
+    test_denoising(test_loader, batch_size, Net, torch.normal(mean=noise_mean,std=noise_std,size=(batch_size,1,image_size,image_size),device='cuda:0'), image_size, local_size)
+elif testType == 'deblurring':
+    pass
---
+++ Test/denoise_test_func.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+sys.path.append(os.path.abspath(os.path.join(__file__,'..','..','Funcs')))
+sys.path.append(os.path.abspath(os.path.join(__file__,'..','..','Nets')))
+
+def test_denoising(test_loader,batch_size,Net,noise,image_size,local_size):
+    for step, (image, label) in enumerate(train_loader):
+        with torch.no_grad():
+            lap_time = image_size // local_size
+            pileNImage = torch.zeros((batch_size * (lap_time ** 2), 3, local_size, local_size)).cuda()
+            image = image.cuda()
+            Nimage = image + noise
+
+            for ii in range(lap_time ** 2):
+                pileNImage[ii * batch_size:(ii + 1) * batch_size, :, :, :] = Nimage[:, :,
+                                                                                (ii // lap_time) * local_size:((
+                                                                                                                           ii // lap_time) + 1) * local_size,
+                                                                                (ii % lap_time) * local_size:((
+                                                                                                                          ii % lap_time) + 1) * local_size]
+            output_done = torch.zeros(batch_size, 3, image_size, image_size).cuda()
+            for i in range(3):
+                done = Net(pileNImage[:, i:i + 1, :, :])
+                for ii in range(lap_time ** 2):
+                    output_done[:, i:i + 1, (ii // lap_time) * local_size:((ii // lap_time) + 1) * local_size,
+                    (ii % lap_time) * local_size:((ii % lap_time) + 1) * local_size] = done[ii * batch_size:(ii + 1) * batch_size, :, :, :]
+            before = sum([-10 * np.log10((torch.norm(maskedimage[i] - image[i], 'fro').item())
+                                         ** 2 / (3 * image_size * image_size)) for i in range(batch_size)]) / batch_size
+            after = sum([-10 * np.log10((torch.norm(output_done[i] - image[i], 'fro').item())
+                                        ** 2 / (3 * image_size * image_size)) for i in range(batch_size)]) / batch_size
+
+        print('Before PSNR: {}, \t After PSNR: {}'.format(before, after))
+
+        plt.imshow(torch.permute(image[0].cpu(), (1, 2, 0)))
+        plt.show()
+        plt.imshow(torch.permute((Nimage[0].cpu()), (1, 2, 0)))
+        plt.show()
+        plt.imshow((np.transpose((output_done.cpu().detach().numpy()[0]), (1, 2, 0))))
+        plt.show()
