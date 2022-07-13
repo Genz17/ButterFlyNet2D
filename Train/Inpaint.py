c6863c8bed90ba1c9e9317344d5886176def91e7
Genz17 <genzyang17@gmail.com>
2022-07-14 02:53:40 +0800

    inpaint preft

--- Train/Inpaint.py
+++ Train/Inpaint.py
@@ -15,17 +15,18 @@ from inpaint_test_func import test_inpainting
 
 
 #### Here are the settings to the training ###
-epochs = 10
-batch_size_train = 25
+epochs = 5
+batch_size_train = 50
 batch_size_test = 256
-learning_rate = 0.001
+learning_rate = 0.002
 data_path_train = '../../data/celebaselected/' # choose the path where your data is located
 data_path_test = '../../data/CelebaTest/' # choose the path where your data is located
-image_size = 128 # the image size
+image_size = 64 # the image size
 local_size = 64 # size the network deals
 pile_time = image_size // local_size
 net_layer = 6 # should be no more than log_2(local_size)
-cheb_num = 4
+cheb_num = 2
+pre = False
 mask_train = eval('squareMask'+str(image_size))(torch.zeros(batch_size_train,1,image_size,image_size)).cuda()
 mask_test = eval('squareMask'+str(image_size))(torch.zeros(batch_size_test,3,image_size,image_size)).cuda()
 
@@ -45,7 +46,7 @@ test_loader = DataLoader(
     batch_size=batch_size_test, shuffle=False)
 
 print('Generating Net...')
-Net = ButterFlyNet_INPAINT(local_size,net_layer,cheb_num,True).cuda()
+Net = ButterFlyNet_INPAINT(local_size,net_layer,cheb_num,False).cuda()
 print('Done.')
 
 num = 0
@@ -54,13 +55,13 @@ for para in Net.parameters():
 print('The number of paras in the network is {}.'.format(num))
 
 # Pre FT Approx:
-Net.preFT(1000)
+if pre:
+    Net.preFT(200)
 
 optimizer = torch.optim.Adam(Net.parameters(), lr=learning_rate)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.98, patience=100, verbose=True,
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.99, patience=100, verbose=True,
                                                          threshold=0.00005, threshold_mode='rel', cooldown=3, min_lr=0, eps=1e-16)
 ##############################################
-
 
 print('Test before training...')
 # Apply one test before training
@@ -90,7 +91,7 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
         scheduler.step(loss)
-        print('Inpaint: local size {}, image size {} Train Epoch: {}/{}, [{}/{} ({:.2f}%)]\tLoss: {:.6f}'.format(local_size,image_size,epoch+1,epochs,step * len(image),
+        print('false Inpaint: local size {}, image size {} Train Epoch: {}/{}, [{}/{} ({:.2f}%)]\tLoss: {:.6f}'.format(local_size,image_size,epoch+1,epochs,step * len(image),
                                                                         len(train_loader.dataset),
                                                                         100 * step / len(train_loader),
                                                                         loss.item()))
