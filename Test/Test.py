a0a71ed80ba5f137b649e8bc8ba21e72df0e8760
Genz17 <genzyang17@gmail.com>
2022-08-08 00:49:48 +0800

    offer picture choice in test

--- Test/Test.py
+++ Test/Test.py
@@ -1,11 +1,15 @@
 import sys
 import os
 import torch
+import math
 import numpy as np
+import matplotlib.pyplot as plt
 sys.path.append(os.path.abspath(os.path.join(__file__,'..','..','Funcs')))
 sys.path.append(os.path.abspath(os.path.join(__file__,'..','..','Nets')))
+from Loader import load_dataset
+from ButterFlyNet_Identical import ButterFlyNet_Identical
 
-def test(test_loader,batch_size,Net,image_size,local_size):
+def test(test_loader,batch_size,Net,image_size,local_size,pic=False):
     for step, (Totalimage, label) in enumerate(test_loader):
         try:
             with torch.no_grad():
@@ -34,6 +38,53 @@ def test(test_loader,batch_size,Net,image_size,local_size):
                 after = sum([-10 * np.log10((torch.norm(output_done[i:i+1,:,:,:] - image[i:i+1,:,:,:], 'fro').item())
                                             ** 2 / (3 * image_size * image_size)) for i in range(batch_size)]) / batch_size
             print('Before PSNR: {}, \t After PSNR: {}'.format(before, after))
+
+            if pic:
+                fig = plt.figure()
+                plt.imshow(torch.permute(image[0].cpu(),(1,2,0)))
+                plt.savefig(imgpath_origin)
+                fig = plt.figure()
+                plt.imshow(torch.permute(operatedimage[0].cpu(),(1,2,0)))
+                plt.savefig(imgpath_operated)
+                fig = plt.figure()
+                plt.imshow(torch.permute(output_done[0].cpu(),(1,2,0)))
+                plt.savefig(imgpath_recover)
+                break
+
         except Exception:
             pass
+
+
+if __name__ == '__main__':
+    task        = sys.argv[1]
+    datasetName = int(sys.argv[2])
+    image_size  = int(sys.argv[3])
+    local_size  = int(sys.argv[4])
+    prefix      = eval(sys.argv[5])
+    pretrain    = eval(sys.argv[6])
+    pic         = eval(sys.argv[7])
+    if prefix:
+        p1 = 'prefix'
+    else:
+        p1 = 'noprefix'
+    if pretrain:
+        p2 = 'pretrain'
+    else:
+        p2 = 'nopretrain'
+
+    batch_size_test = 256
+    net_layer       = int(math.log2(local_size))
+    cheb_num        = 2
+    pthpath = '../../Pths/' + task + '/' + p1 + '/' + p2 + '/' + datasetName + '_{}_{}_{}_{}.pth'.format(local_size,image_size,net_layer,cheb_num)
+    imgpath_origin = '../../Images/pics/' + task + '/' + p1 + '/' + p2 + '/' + datasetName + '_{}_{}_{}_{}_or.eps'.format(local_size,image_size,net_layer,cheb_num)
+    imgpath_operated = '../../Images/pics/' + task + '/' + p1 + '/' + p2 + '/' + datasetName + '_{}_{}_{}_{}_op.eps'.format(local_size,image_size,net_layer,cheb_num)
+    imgpath_recover = '../../Images/pics/' + task + '/' + p1 + '/' + p2 + '/' + datasetName + '_{}_{}_{}_{}_re.eps'.format(local_size,image_size,net_layer,cheb_num)
+
+    checkPoint = torch.load(pthpath)
+    Net = ButterFlyNet_Identical(local_size,net_layer,cheb_num).cuda()
+    Net.load_state_dict(checkPoint['Net'])
+    train_loader,test_loader = load_dataset(task, datasetName, 20, batch_size_test, image_size, local_size, p1, p2)
+    test(test_loader,batch_size_test,Net,image_size,local_size,pic)
+
+
 
